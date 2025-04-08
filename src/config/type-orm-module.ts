57489c8edb180297cee9206b4ac7b0b375d64294
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-04-08 22:19:48 +0300

    fix: type-orm config

--- src/config/type-orm-module.ts
+++ src/config/type-orm-module.ts
@@ -1,7 +1,9 @@
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { User } from 'src/users/entities/user.entity';
+import { User } from '../users/entities/user.entity';
+import { Role } from '../roles/entities/role.entity';
+import { UserRole } from '../user-roles/entities/user-role.entity';
 
 export const typeOrmModule = TypeOrmModule.forRootAsync({
   imports: [ConfigModule],
@@ -12,7 +14,7 @@ export const typeOrmModule = TypeOrmModule.forRootAsync({
     username: configService.get('POSTGRES_USER') ?? 'postgres',
     password: configService.get('POSTGRES_PASSWORD') ?? 'newPassword',
     database: configService.get('POSTGRES_DB') ?? 'nestplaces',
-    entities: [User],
+    entities: [User, Role, UserRole],
     synchronize: true,
   }),
   inject: [ConfigService],
