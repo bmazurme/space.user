40743560ed0aaa14ac4cf42d4b2c4acfa54ead9a
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-04-08 22:24:07 +0300

    fix: app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,16 @@ import { ConfigModule } from '@nestjs/config';
 
 import { UsersModule } from './users/users.module';
 import { typeOrmModule } from './config/type-orm-module';
+import { UserRolesModule } from './user-roles/user-roles.module';
+import { RolesModule } from './roles/roles.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     typeOrmModule,
     UsersModule,
+    UserRolesModule,
+    RolesModule,
   ],
 })
 export class AppModule {}
