cac6e33d7f62fa549841d56acd2a29e1c4d90bae
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-04-09 17:46:53 +0300

    fix: main

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions } from '@nestjs/microservices';
+
 import { AppModule } from './app.module';
 import { grpcClientOptions } from './grpc-client.options';
 
